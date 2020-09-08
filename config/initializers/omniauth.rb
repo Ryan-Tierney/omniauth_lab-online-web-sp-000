b51cab46c91d5ff1ec12b5e76fbe966b6c26b484
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-08 03:04:49 +0000

    Automatically backed up by Learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  #provider :github, ENV['GITHUB_KEY=f93024e7e4992092a54a'], ENV['GITHUB_SECRET=222dc9e2113082fe08746e30c04342bf6d00d996']
+  provider :github, ENV['GITHUB_KEY=f93024e7e4992092a54a'], ENV['GITHUB_SECRET=222dc9e2113082fe08746e30c04342bf6d00d996']
 end
